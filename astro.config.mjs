a373953ab6ef0bfd304687355da799fcbdaf33e3
Dav Co <iksvdx@zohomail.eu>
2026-07-29 01:31:41 +0200

    - added cloudflare and react integration - moved from json to md for the books content -  created dynamic books pages from the md files - created a products page to simulate ssr rendering with dummy json api - added a react component for the likes to simulate island hydration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 
-// https://astro.build/config
-export default defineConfig({});
+import react from '@astrojs/react';
+
+export default defineConfig({
+  adapter: cloudflare(),
+  integrations: [react()],
+});
